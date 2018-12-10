b42818455aeca6a9639685e48566b556f19dac46
Alessandro Nardini <42110232+alexnardini@users.noreply.github.com>
2018-12-09 18:45:12 -0800

    Added python code into the proper folder

---
+++ include/PYTHON/FFX_NSIMcount_recount.py
@@ -0,0 +1,41 @@
+## FFX_IMPORT v1.5
+## SideFX Houdini OTL
+##
+## Author/Copyrights: Alessandro Nardini
+## Email: alexnardini@gmail.com
+## Creation date: 09th October 2018
+##
+## "FFX_import" is a little houdini OTL to work
+## with FumeFX caches inside Houdini in an easy way.
+## It works like "DOP import fields" node does for Pyro
+## but instead of referencing a live DOP
+## it will operate on simulation VDB caches
+## done with FumeFX v5 and up.
+##
+
+node = hou.pwd()
+geo = node.geometry()
+
+inputs = node.inputs()
+geopython = inputs[1].geometry()
+
+try:
+    nsimcount = geopython.intAttribValue("FFXNSIMcount")
+    FFXNSIMcountcheck = 0
+    
+    for x in range(nsimcount):
+        myfile = geopython.stringAttribValue("FFXNSIMVDBrefintframe"+str(x))
+        geo.loadFromFile(myfile)
+        nsimcount = geo.intAttribValue("NSimCount")
+        if nsimcount>=1:
+            FFXNSIMcountcheck += 1
+        geo.clear()
+        
+    geo.addAttrib(hou.attribType.Global, "FFXNSIMcountcheck", 0)
+    
+    if FFXNSIMcountcheck>1:
+        geo.setGlobalAttribValue("FFXNSIMcountcheck", 1)
+    else:
+        geo.setGlobalAttribValue("FFXNSIMcountcheck", 0)
+except:
+    pass
